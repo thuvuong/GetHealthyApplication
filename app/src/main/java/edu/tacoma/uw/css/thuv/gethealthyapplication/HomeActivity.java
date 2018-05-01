a69cbab74e38df9032e820b1caecd0e6782d3857
brookn3 <brooknegussie2014@gmail.com>
2018-04-30 22:13:26 -0700

    removed floating action button

--- app/src/main/java/edu/tacoma/uw/css/thuv/gethealthyapplication/HomeActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/thuv/gethealthyapplication/HomeActivity.java
@@ -1,11 +1,7 @@
 package edu.tacoma.uw.css.thuv.gethealthyapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
