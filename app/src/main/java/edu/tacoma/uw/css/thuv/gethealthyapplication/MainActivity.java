da20334615f1004e9fe7956bc40d7a43f72b1323
yulinw <35405077+yulinw@users.noreply.github.com>
2018-05-08 00:10:19 -0700

    Redo the registration.

--- app/src/main/java/edu/tacoma/uw/css/thuv/gethealthyapplication/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/thuv/gethealthyapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.tacoma.uw.css.thuv.gethealthyapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,8 +12,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements
+            RegistrationFragment.UserAddListener{
+
+    @Override
+    public void addUser(String url){
+        AddUserTask task = new AddUserTask();
+        task.execute(new String[]{url.toString()});
+
+        getSupportFragmentManager().popBackStackImmediate();
+    }
 
     private EditText mEmail;
     private EditText mPassword;
@@ -58,12 +76,76 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mCreateNewAccount = (Button) findViewById(R.id.create_new_account);
+        mCreateNewAccount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                RegistrationFragment rf = new RegistrationFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_activity, rf, null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 
     public void login(final String theEmail, final String thePassword) {
         Intent i = new Intent(this, HomeActivity.class);
         startActivity(i);
         finish();
+    }
+
+    private class AddUserTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls){
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls){
+                try{
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while((s = buffer.readLine()) != null){
+                        response += s;
+                    }
+                }catch(Exception e){
+                    response = "Unable to register. Reason: "
+                            + e.getMessage();
+                }finally {
+                    if (urlConnection != null){
+                        urlConnection.disconnect();
+                    }
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            try{
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if(status.equals("success")){
+                    Toast.makeText(getApplicationContext(), "Successfully Registered!",
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Failed to register",
+                            Toast.LENGTH_LONG).show();
+                }
+            }catch(JSONException e){
+                Toast.makeText(getApplicationContext(), "Something wrong with the data" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
